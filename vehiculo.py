570718ac3b5a169c82d20b2afd2d544f9b538117
Luis <luis12035@gmail.com>
2020-03-13 18:27:47 -0600

    Archivo vehiculo ultima version

--- vehiculo.py
+++ vehiculo.py
@@ -1,16 +1,27 @@
 #-*- coding: utf8 -*-
 
-import datetime
+from datetime import datetime, timedelta
+from random import randint
+
+
+vehiculo_id = 0
 
 class Vehiculo():
     """ Representa un vehiculo estacionado en el parqueo"""
 
-    def __init__(self, placa, marca, modelo):
+
+    def __init__(self, placa, marca, modelo, tipo):
         """ Ddefinimos las caracteristicas del objeto """
         self.placaVehiculo = placa
         self.marcaVehiculo = marca
         self.modeloVehiculo = modelo
-        self.horaIngreso = datetime.time.hour()
+        self.tipoVehiculo = tipo
+        global vehiculo_id
+        vehiculo_id += 1
+        self.idVehiculo = vehiculo_id
+        self.horaIngreso = datetime.now()
+        self.horaSalida = timedelta(randint(1,5))
         self.estado = True
+
 
     
