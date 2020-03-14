dce0d5854bd4a3737e527a4221df448ce6eb56a2
Luis <luis12035@gmail.com>
2020-03-13 18:26:37 -0600

    Archivo de parking ultima version

---
+++ parkin.py
@@ -0,0 +1,43 @@
+# -*- coding: utf8 -*-
+
+from vehiculo import Vehiculo
+
+
+class Parking():
+    """
+    
+    """
+
+    def __init__(self):
+        """ 
+        
+        """
+        self.vehiculo = list()
+
+    def new_vehiculo(self, placa, marca, modelo, tipo):
+        """" 
+        Metodo de llenado para la lista que 
+        contendra la informacion de los vehiculos en el parqueo
+        """
+        self.vehiculo.append(Vehiculo(placa,marca,modelo,tipo))
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+    
+
+
+
